883f8ad87a4399b1a34040d63f05da21c2bcb39d
Andrew <Andreiks@inbox.ru>
2024-07-21 22:37:08 +0600

    move configuration to Extensions in minApi project

--- Habr.WebApp.MinimalApi/Program.cs
+++ Habr.WebApp.MinimalApi/Program.cs
@@ -52,63 +52,9 @@ namespace Habr.WebApp.MinimalApi
 
             builder.Services.AddEndpointsApiExplorer();
 
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo { Title = "HabrAPI", Version = "v1" });
+            builder.Services.ConfigureSwagger();
 
-                options.OperationFilter<SwaggerResponseCodesFilter>();
-
-                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "Provide token in format:   Bearer \\<your token\\>",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-
-                options.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        Array.Empty<string>()
-                    }
-                });
-            });
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                        ValidAudience = builder.Configuration["Jwt:Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-                    };
-                    options.Events = new JwtBearerEvents
-                    {
-                        OnAuthenticationFailed = context =>
-                        {
-                            Console.WriteLine("Authentication failed: " + context.Exception.Message);
-                            return Task.CompletedTask;
-                        },
-                        OnChallenge = context =>
-                        {
-                            Console.WriteLine("Challenge: " + context.Error);
-                            return Task.CompletedTask;
-                        }
-                    };
-                });
+            builder.ConfigureAuthentication();
 
             builder.Services.AddAuthorization();
 
