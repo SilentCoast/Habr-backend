379539594e31455ccca6e35623897d69d27712b4
Andrew <Andreiks@inbox.ru>
2024-08-13 21:48:07 +0600

    NB2024-197
    Posts pagination

--- Habr.DataAccess/DTOs/PublishedPostDTO.cs
+++ Habr.DataAccess/DTOs/PublishedPostDTO.cs
@@ -5,7 +5,9 @@
         public int PostId { get; set; }
         public string Title { get; set; }
         public string AuthorEmail { get; set; }
-        //TODO: rename to PublishedAt in the next api version
+        /// <summary>
+        /// renamed to PublishedAt in <see cref="PublishedPostV2Dto.PublishedAt"/>
+        /// </summary>
         public DateTime PublishDate { get; set; }
     }
 }
